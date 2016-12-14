c9132d1699ddfe4389cadc037226353211bdce5f
4851610 <4851610@hanmail.net>
2016-12-14 14:32:33 +0900

    commit

--- JSPProject/src/review/ReviewDAO.java
+++ JSPProject/src/review/ReviewDAO.java
@@ -3,16 +3,29 @@ package review;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import util.DBUtil;
 
 public class ReviewDAO {
 	Connection conn;
 	PreparedStatement pstmt;
 	ResultSet rs;
 	ArrayList<Review> list = new ArrayList<Review>();
-	  
+	String sql;
+
 	public ArrayList<Review> list() {
+		conn = DBUtil.connection();
+		sql = "";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+
+		}
 		return list;
 	}
 }
-  
